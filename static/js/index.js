e3470f1135a9095d354b235ad0eb53c554193fae
Silverster98 <1539168414@qq.com>
2024-04-17 22:17:44 +0800

    Update

--- static/js/index.js
+++ static/js/index.js
@@ -4,27 +4,23 @@ Vue.component('my-publication', {
 	<div class="box">
 		<div class="columns">
 			<div class="column is-3">
-				<img src="" style="width:100%; height:auto" v-bind:src="pub.image"/>
+				<img style="width:100%; height:auto" v-bind:src="pub.image"/>
 			</div>
 
 			<div class="column is-9" v-bind:style="pub.bgColor">
 				<div class="block">
-					<p class="paper-title">{{ pub.title }}</p>
-					<p class="paper-author">{{ pub.authors }}</p>
-					<p class="paper-publisher"><span v-html="pub.publisher"></span></p>
+					<div class="paper-title"><span>{{ pub.title }}</span></div>
+					<div class="paper-author"><span>{{ pub.authors }}</span></div>
+					<div class="paper-publisher"><span v-html="pub.publisher"></span></div>
+					<div class="paper-links">
+						<a v-bind:href="pub.paper" target="_blank">Paper</a>
+						&nbsp;&nbsp;
+						<a v-bind:href="pub.video" target="_blank">Video</a>
+						&nbsp;&nbsp;
+						<a v-bind:href="pub.project" target="_blank">Website</a>
+					</div>
+					<p class="paper-description">{{ pub.description }}</>
 				</div>
-
-				<a v-bind:href="pub.paper" target="_blank" style="font-weight:bold; vertical-align: middle; color:#787878">
-					<img alt="Paper" src="./static/images/img/paper.png" border="0" title="Paper"></a>
-				&nbsp;&nbsp;&nbsp;&nbsp;
-				<a v-bind:href="pub.video" target="_blank" style="font-weight:bold; vertical-align: middle; color:#787878">
-					<img alt="Video" src="./static/images/img/video.png" border="0" title="Video"></a>
-				&nbsp;&nbsp;&nbsp;&nbsp;
-				<a v-bind:href="pub.presentation" target="_blank" style="font-weight:bold; vertical-align: middle; color:#787878">
-					<img alt="Presentation" src="./static/images/img/ppt.png" border="0" title="Presentation"></a>
-				&nbsp;&nbsp;&nbsp;&nbsp;
-				<a v-bind:href="pub.project" target="_blank" style="font-weight:bold; vertical-align: middle; color:#787878">
-					<img alt="Project Page" src="./static/images/img/home.png" border="0" title="Project"></a>
 			</div>
 		</div>
 	</div>`,
