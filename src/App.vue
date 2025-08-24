eb3dc7e07a5972c564f1f211b42d7badd0b585ca
Silverster98 <1539168414@qq.com>
2025-08-24 20:00:10 +0800

    Update main version 3

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+
+<div class="min-h-screen flex flex-col">
+  <div class="w-full max-w-6xl mx-auto p-6 md:p-12 flex-grow flex flex-col items-center justify-center text-center">
+    <div class="flex flex-col justify-center items-center">
+      <h2 class="text-4xl font-bold p-3 md:p-6" style="font-family: 'Trebuchet MS', 'Geneva CE', lucida, sans-serif;">
+        <strong>Wei Liang</strong>
+      </h2>
+      <img src="/images/liangwei.png" alt="Logo" class="size-32 rounded-full justify-center items-center mx-auto">
+    </div>
+
+    <div class="text-md px-12 md:px-16 py-6 md:py-8 text-justify text-center leading-normal">
+      I am a professor in the <a href="https://cs.bit.edu.cn/" class="secondary-link" target="_blank"><strong>School of Computer Science & Technology</strong></a> at <a href="https://bit.edu.cn/" class="secondary-link" target="_blank"><strong>Beijing Institute of Technology (BIT)</strong></a>, leading the <a href="https://pie-lab.cn/" class="secondary-link" target="_blank"><strong>Perception, Interaction, and Embodiment Lab (PIE Lab)</strong></a>.
+
+      I received my Ph.D in computer science from BIT in 2005. I worked as a visiting professor at <strong><a href="http://www.stat.ucla.edu/~sczhu/" target="_blank" class="secondary-link">Song-Chun Zhu</a></strong>'s Vision Cognition Learning and Autonomous (VCLA) in 2014 and 2015. My research interests lie in the field of Computer Vision, Statistical Inference, VR for training and education, and Cognitive Science.
+      <br>
+      <br>
+
+      <strong>For more information about my team, research, and publication, please visit my lab website: <a href="https://pie-lab.cn/" class="primary-link italic" target="_blank"> https://pie-lab.cn/ </a></strong>.
+      <br>
+      <br>
+
+      <strong>Email: liangwei[at]bit[dot]edu[dot]cn</strong>
+
+    </div>
+  </div>
+
+  <footer class="w-full max-w-6xl mx-auto p-3 md:p-6 text-center text-neutral-700">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div class="flex justify-center">
+        <img src="/bit_logo.svg" alt="Logo" class="size-16">
+        <div class="text-sm ml-6 py-2">
+          <p class="">
+            感知、交互与具身实验室
+            <br>
+            Perception, Interaction, and Embodiment Lab (PIE Lab)
+          </p>
+        </div>
+      </div>
+      <div class="text-sm py-2 ">
+         <p style="font-family:Times,Georgia,New Century Schoolbook">
+          Beijing Institute of Technology Copyright &nbsp;&nbsp;&nbsp; Address: 5 South Zhongguancun
+          <br>
+          Street, Haidian District, Beijing, China 100081
+        </p>
+      </div>
+    </div>
+  </footer>
+</div>
+
+</template>
